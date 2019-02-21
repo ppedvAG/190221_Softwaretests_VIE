ca1e3fd6988a5a7da41d345ebef74d8707bc7e5c
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-21 10:17:32 +0100

    Erster UnitTest

---
+++ TaschenrechnerLib/TaschenrechnerLib.MSTest.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TaschenrechnerLib.MSTest.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TaschenrechnerLib.MSTest.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d048ff06-399a-4ffd-a842-5ccc83f63c5b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TaschenrechnerLib/TaschenrechnerLib.MSTest.Tests/TaschenrechnerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaschenrechnerLib.MSTest.Tests
+{
+    [TestClass]
+    public class TaschenrechnerTests
+    {
+        [TestMethod]
+        public void Taschenrechner_Add_5_and_3_results_8()
+        {
+            var t = new TaschenrechnerLib.Taschenrechner();
+
+            int erg = t.Add(5, 3);
+
+            Assert.AreEqual(8, erg);
+        }
+    }
+}
