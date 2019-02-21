4578b70b886d662b6bf5642718252f74df42529b
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-21 10:03:56 +0100

    Der komplexeste Taschenrechner der Welt ;)

--- TaschenrechnerLib/TaschenrechnerLib/Class1.cs
+++ TaschenrechnerLib/TaschenrechnerLib/Class1.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace TaschenrechnerLib
 {
-    public class Class1
+    public class Taschenrechner
     {
+        public int Add(int z1, int z2)
+        {
+            return z1 + z2;
+        }
     }
 }
