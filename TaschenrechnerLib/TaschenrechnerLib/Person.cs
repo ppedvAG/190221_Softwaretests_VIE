8454f17caf84823db281ddcab79aec8d5a3aeded
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-21 11:55:42 +0100

    Übung: UnitTests für Person.Equals() schreiben

---
+++ TaschenrechnerLib/TaschenrechnerLib/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaschenrechnerLib
+{
+    class Person
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public byte Alter { get; set; }
+        public decimal Kontostand { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            // Übung:
+            // False wenn:
+            //       Unterschiedliche Datentypen
+            //       Null
+            // True wenn:
+            //       Referenzen gleich
+            //       Datentyp gleich und Werte gleich (easy: Vorname == obj.Vorname)
+
+            throw new NotImplementedException();
+        }
+    }
+}
