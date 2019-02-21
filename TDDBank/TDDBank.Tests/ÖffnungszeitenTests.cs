a723abcc61c5a355c433b235e93d2f95eb681017
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-21 14:45:51 +0100

    Overkill-Variante für Öffnungszeiten

---
+++ TDDBank/TDDBank.Tests/ÖffnungszeitenTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDBank.Tests
+{
+    [TestClass]
+    public class ÖffnungszeitenTests
+    {
+        [TestMethod]
+        [DataRow(2019,2,18,12,31,true)] //Mo - True
+        [DataRow(2019,2,20,18,00,true)] //Mi - True
+        [DataRow(2019,2,22,10,21,false)] //Fr - False
+        [DataRow(2019,2,23,12,31,true)] //Sa - True
+        [DataRow(2019,2,23,14,5,false)] //Sa - False
+        // Extremfälle
+        [DataRow(2019, 2, 20, 10, 30, true)] //Mi - True
+        [DataRow(2019, 2, 20, 19, 00, false)]//Mi - False
+        [DataRow(2019, 2, 23, 10, 30, true)] //Sa - True
+        [DataRow(2019, 2, 23, 14, 00, false)] //Sa - False
+        public void Öffnungszeiten_IsOpen(int jahr,int monat,int tag, int stunde, int minute, bool erwartetesResultat)
+        {
+            var d = new DateTime(jahr, monat, tag, stunde, minute, 0);
+            var ö = new Öffnungszeiten();
+
+            Assert.AreEqual(erwartetesResultat, ö.IsOpen(d));
+        }
+    }
+}
---
+++ TDDBank/TDDBank/Öffnungszeiten.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDBank
+{
+    struct Zeiten
+    {
+        public Zeiten(TimeSpan offenAb, TimeSpan zuAb) : this()
+        {
+            OffenAb = offenAb;
+            ZuAb = zuAb;
+        }
+
+        public TimeSpan OffenAb { get; set; }
+        public TimeSpan ZuAb { get; set; }
+    }
+
+    public class Öffnungszeiten
+    {
+        public Öffnungszeiten()
+        {
+            tage.Add(DayOfWeek.Monday, new Zeiten(new TimeSpan(10, 30, 00), new TimeSpan(19, 00, 00)));
+            tage.Add(DayOfWeek.Tuesday, new Zeiten(new TimeSpan(10, 30, 00), new TimeSpan(19, 00, 00)));
+            tage.Add(DayOfWeek.Wednesday, new Zeiten(new TimeSpan(10, 30, 00), new TimeSpan(19, 00, 00)));
+            tage.Add(DayOfWeek.Thursday, new Zeiten(new TimeSpan(10, 30, 00), new TimeSpan(19, 00, 00)));
+            tage.Add(DayOfWeek.Friday, new Zeiten(new TimeSpan(10, 30, 00), new TimeSpan(19, 00, 00)));
+            tage.Add(DayOfWeek.Saturday, new Zeiten(new TimeSpan(10, 30, 00), new TimeSpan(14 ,00, 00)));
+        }
+        private Dictionary<DayOfWeek, Zeiten> tage = new Dictionary<DayOfWeek, Zeiten>();
+
+        public bool IsOpen(DateTime time)
+        {
+            return tage.ContainsKey(time.DayOfWeek) &&
+                   tage[time.DayOfWeek].OffenAb <= time.TimeOfDay &&
+                   tage[time.DayOfWeek].ZuAb > time.TimeOfDay;
+        }
+    }
+}
