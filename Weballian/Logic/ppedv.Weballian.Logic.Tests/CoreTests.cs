ece56a4deb69064427b285a7015e25e63bb7f3ce
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-22 14:08:21 +0100

    Mocking mit Moq

--- Weballian/Logic/ppedv.Weballian.Logic.Tests/CoreTests.cs
+++ Weballian/Logic/ppedv.Weballian.Logic.Tests/CoreTests.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ppedv.Weballian.Domain;
+using ppedv.Weballian.Domain.Interfaces;
 
 namespace ppedv.Weballian.Logic.Tests
 {
@@ -9,11 +13,50 @@ namespace ppedv.Weballian.Logic.Tests
         [TestMethod]
         public void Core_generate_Movies_with_negative_amount_throws_ArgumentException()
         {
-            // Was passiert, wenn ich das Gerät nicht habe ?
-            Core core = new Core(null); // ich brauche ein "gerät" zum testen D: -> Moq
+            var mock = new Mock<IDevice>();
+            Core core = new Core(mock.Object,null); // repo wird nicht gebraucht
 
             // Was macht mein echtes Gerät, wenn die Anzahl nicht stimmt ?
             Assert.ThrowsException<ArgumentException>(() => core.GenerateMovies(-20));
+
+            mock.Verify(x => x.ProduceMovie(), Times.Never());
+        }
+
+        [TestMethod]
+        public void Core_can_generate_5_Movies()
+        {
+            var mock = new Mock<IDevice>();
+            Core core = new Core(mock.Object,null); // repo wird nicht gebraucht
+
+            core.GenerateMovies(5); // Wie kann ich jetzt quasi prüfen, dass die Methode .ProduceMovie() aufgerufen wurde ?
+
+            mock.Verify(x => x.ProduceMovie(), Times.Exactly(5));
+        }
+
+        [TestMethod]
+        public void Core_can_GetPeopleWithAgeGreaterThan100()
+        {
+            // Typische Businesslayer-Logik: Hol was aus der DB und mach was mit den Daten
+            var mock = new Mock<IRepository>();
+
+            // Setup: Wir geben die Ergebnisse vor:
+            mock.Setup(x => x.GetAll<Person>())
+                .Returns(() =>
+                {
+                    return new Person[]
+                    {
+                        new Person{FirstName="Tom",LastName="Ate",Age=110,Balance=100},
+                        new Person{FirstName="Anna",LastName="Nass",Age=120,Balance=200},
+                        new Person{FirstName="Peter",LastName="Silie",Age=130,Balance=300},
+                        new Person{FirstName="Martha",LastName="Pfahl",Age=20,Balance=300},
+                        new Person{FirstName="Klara",LastName="Fall",Age=67,Balance=123123300}
+                    };
+                });
+
+            Core core = new Core(null,mock.Object); // device wird nicht benötigt
+            var result = core.GetPeopleWithAgeGreaterThan100();
+
+            Assert.AreEqual(3, result.Count());
         }
     }
 }
--- Weballian/Logic/ppedv.Weballian.Logic/Core.cs
+++ Weballian/Logic/ppedv.Weballian.Logic/Core.cs
@@ -1,15 +1,20 @@
-﻿using ppedv.Weballian.Domain.Interfaces;
+﻿using ppedv.Weballian.Domain;
+using ppedv.Weballian.Domain.Interfaces;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ppedv.Weballian.Logic
 {
     public class Core
     {
-        public Core(IDevice device)
+        public Core(IDevice device, IRepository repository)
         {
             Device = device;
+            Repository = repository;
         }
         public IDevice Device { get; set; }
+        public IRepository Repository { get; set; }
 
         public void GenerateMovies(int amount) // Business-Logik, die ein "Gerät" verwendet
         {
@@ -19,6 +24,11 @@ namespace ppedv.Weballian.Logic
             for (int i = 0; i < amount; i++)
                 Device.ProduceMovie();
             // Ziel: Businesslogik testen, ohne dass das "echte" Gerät genutzt wird
+        }
+
+        public IEnumerable<Person> GetPeopleWithAgeGreaterThan100()
+        {
+            return Repository.GetAll<Person>().Where(x => x.Age >= 100);
         }
     }
 }
