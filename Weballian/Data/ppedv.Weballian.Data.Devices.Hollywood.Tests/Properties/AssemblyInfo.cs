bd33808f2e193ff3f9d38767dbafecd502915718
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-22 12:13:36 +0100

    Init Hollywood.Tests

---
+++ Weballian/Data/ppedv.Weballian.Data.Devices.Hollywood.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ppedv.Weballian.Data.Devices.Hollywood.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ppedv.Weballian.Data.Devices.Hollywood.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d8a97ad-1d03-447c-8cc0-231729c35d0a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
