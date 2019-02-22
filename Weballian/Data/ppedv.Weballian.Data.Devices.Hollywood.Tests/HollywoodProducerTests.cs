24746970aeb1a4ebffb22284e2d8c3da364bae72
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-22 12:17:38 +0100

    Problemstellung ohne Mock

---
+++ Weballian/Data/ppedv.Weballian.Data.Devices.Hollywood.Tests/HollywoodProducerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ppedv.Weballian.Data.Devices.Hollywood.Tests
+{
+    [TestClass]
+    public class HollywoodProducerTests
+    {
+        [TestMethod]
+        public void HollywoodProducer_can_produce_movie()
+        {
+            HollywoodProducer.ProduceMovie();
+            // Problem: die echte Maschine "bewegt" sich beim testen des Treibers
+            // Wir müssen den Treiber so testen, dass keine echte Hardware auf Unittests reagiert
+
+            // Lösung: Gerät simulieren -> Mock
+        }
+    }
+}
