efdd4357b74c4f86413329def89f848dcd9822fe
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-22 12:10:51 +0100

    "Gerätetreiber" erstellt

--- Weballian/Data/ppedv.Weballian.Data.Devices.Hollywood/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ppedv.Weballian.Data.Devices.Hollywood
-{
-    public class Class1
-    {
-    }
-}
---
+++ Weballian/Data/ppedv.Weballian.Data.Devices.Hollywood/HollywoodProducer.cs
@@ -0,0 +1,14 @@
+﻿using ACME.Filmgenerator3000;
+using System;
+
+namespace ppedv.Weballian.Data.Devices.Hollywood
+{
+    public class HollywoodProducer
+    {
+        public static void ProduceMovie()
+        {
+            // "Treiber", der intern die Maschine ansteuert
+            Filmgenerator.GenerateMovie();
+        }
+    }
+}
